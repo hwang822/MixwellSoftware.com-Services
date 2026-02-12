3ed8736ec506fb8d679e50c63e393a90dd1e96dc
Henry Wang <hwang822@gmail.com>
2026-02-12 14:29:55 -0600

    update aiservices

--- portal/services/servicesStartup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-python AIService/aiService.py
-python CamServiceService/camService.py
-python EmailService/emailService.py
-python TravelService/travleService.py
-python VideoService/videoService.py
--- project/servicesStartup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-python services/AIService/aiService.py
-python services/CamService/camService.py
-python services/VideoService/videoService.py
